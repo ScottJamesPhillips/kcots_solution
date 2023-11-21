9086c900b59e1ea3f9dbc00f2f1437f8bbbeedf1
scottphillipsmoov <scottjamesphillips@hotmail.com>
2023-11-21 09:25:41 +0000

    Initial Commit

---
+++ Kcots/Kcots/Configuration/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kcots.Configuration
+{
+    class Settings
+    {
+        string avApiKey = "4X0XTW8QRBNKICTL";
+    }
+}
---
+++ Kcots/Kcots/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Kcots.Models;
+using ServiceStack;
+using ServiceStack.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Kcots
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InitSettings()
+        {
+            try
+            {
+                //Configuration.Settings.Load();
+            }
+            catch (Exception) { }
+        }
+
+        private void HideAll()
+        {
+            ctrlTabHome.Visibility = Visibility.Collapsed;
+        }
+
+
+        
+
+        
+
+
+
+    }
+}
