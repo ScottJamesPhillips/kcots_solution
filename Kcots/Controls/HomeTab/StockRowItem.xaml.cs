0f67fe2245dfbb2fe95c5e3764ff97c85e943cde
scottphillipsmoov <scott.phillips@moov.tv>
2023-11-23 17:16:14 +0000

    Expnadable StockRowItem onHover

--- Kcots/Controls/HomeTab/StockRowItem.xaml.cs
+++ Kcots/Controls/HomeTab/StockRowItem.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -24,5 +25,31 @@ namespace Kcots.Controls.HomeTab
         {
             InitializeComponent();
         }
+
+        private void StackPanel_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Border sp = sender as Border;
+            DoubleAnimation db = new DoubleAnimation();
+            //db.From = 12;
+            db.To = 150;
+            db.Duration = TimeSpan.FromSeconds(0.5);
+            db.AutoReverse = false;
+            db.RepeatBehavior = new RepeatBehavior(1);
+            sp.BeginAnimation(StackPanel.HeightProperty, db);
+        }
+
+        private void StackPanel_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Border sp = sender as Border;
+            DoubleAnimation db = new DoubleAnimation();
+            //db.From = 12;
+            //db.To = 12;
+            db.Duration = TimeSpan.FromSeconds(0.5);
+            db.AutoReverse = false;
+            db.RepeatBehavior = new RepeatBehavior(1);
+            sp.BeginAnimation(StackPanel.HeightProperty, db);
+        }
     }
+
 }
+
