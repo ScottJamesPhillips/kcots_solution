43f54da32b5bc1d538ec0534330faa07db2e0b3f
scottphillipsmoov <scottjamesphillips@hotmail.com>
2023-12-13 18:21:57 +0000

    Adding interfaces to solution

---
+++ Kcots/Interfaces/IDataAccess.cs
@@ -0,0 +1,15 @@
+﻿using Kcots.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kcots.Interfaces
+{
+    public interface IDataAccess
+    {
+        Task<List<Stocks>> GetStocks();
+        Task<StocksMarketDataApiResponse> GetMarketDataForStock(string symbol);
+    }
+}
