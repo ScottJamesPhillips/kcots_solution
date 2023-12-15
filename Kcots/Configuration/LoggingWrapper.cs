5244954448657e9c142e6f7b8e68bafe09128692
scottphillipsmoov <scott.phillips@moov.tv>
2023-12-15 15:00:46 +0000

    Remove commented code LoggingWrapper

--- Kcots/Configuration/LoggingWrapper.cs
+++ Kcots/Configuration/LoggingWrapper.cs
@@ -11,14 +11,6 @@ namespace Kcots.Configuration
 {
     public class LoggingWrapper:ILoggerWrapper
     {
-        //public enum LogType
-        //{
-        //    warning,
-        //    info,
-        //    error
-        //}
-
-
         public static ILogger<MainWindow> logger;
 
         public LoggingWrapper()
@@ -27,30 +19,14 @@ namespace Kcots.Configuration
 
         public void InitializeLogger()
         {
+            //Create logger and configure for use
             ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
             {
                 LoggerConfiguration loggerConfiguration = new LoggerConfiguration().WriteTo.File("Logs.txt");
                 builder.AddSerilog(loggerConfiguration.CreateLogger());
             });
-            //Microsoft.Extensions.Logging.ILogger testingComman = loggerFactory.CreateLogger
-           logger = loggerFactory.CreateLogger<MainWindow>();  
+            logger = loggerFactory.CreateLogger<MainWindow>();
         }
-
-        //public static void WriteLog(string message, LogType lt)
-        //{
-        //    switch (lt)
-        //    {
-        //        case LogType.error:
-        //            logger.LogError(message);
-        //            break;
-        //        case LogType.info:
-        //            logger.LogInformation(message);
-        //            break;
-        //        case LogType.warning:
-        //            logger.LogWarning(message);
-        //            break;
-        //    }
-        //}
 
         public void LogInformation(string message) {
             logger.LogInformation(message);
