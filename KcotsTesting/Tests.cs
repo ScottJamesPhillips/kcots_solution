08cde0f8325ae33bbbb4e0bf2c8b53b06380e7a1
scottphillipsmoov <scott.phillips@moov.tv>
2023-12-15 15:01:41 +0000

    Tests for GetStocks using interfaces

--- KcotsTesting/Tests.cs
+++ KcotsTesting/Tests.cs
@@ -8,12 +8,62 @@ using Kcots.Configuration;
 using Kcots.Data;
 using Moq;
 using Microsoft.Extensions.Logging;
+using Kcots.Interfaces;
+using System.Net.Http;
+using System.Net;
+using Newtonsoft.Json;
 
 namespace KcotsTesting
 {
     [TestClass]
     public class Tests
     {
+        [TestMethod]
+        public async Task GetStocks_ShouldNotBeEmpty_WhenHttpResponseIsSuccessful()
+        {
+            // Arrange
+            Mock<ILoggerWrapper> loggerMock = new Mock<ILoggerWrapper>();
+            Mock<IHttpClientWrapper> httpClientMock = new Mock<IHttpClientWrapper>();
+            DataAccess dataAccess = new DataAccess(loggerMock.Object, httpClientMock.Object);
+            List<Stocks> expectedStocks = new List<Stocks> {new Stocks {}};
+            HttpResponseMessage httpResponse = new HttpResponseMessage(HttpStatusCode.OK);
+            var responseJsonString = JsonConvert.SerializeObject(new StocksApiResponse { Data = expectedStocks });
+            httpResponse.Content = new StringContent(responseJsonString);
+            httpClientMock.Setup(client => client.SendAsync(It.IsAny<HttpRequestMessage>()))
+                          .ReturnsAsync(httpResponse);
+
+            // Act
+            List<Stocks> result = await dataAccess.GetStocks();
+
+            // Assert
+            Assert.IsTrue(result.Count > 0);
+            loggerMock.Verify(logger => logger.LogInformation(It.IsAny<string>()), Times.Once);
+            httpClientMock.Verify(client => client.SendAsync(It.IsAny<HttpRequestMessage>()), Times.Once);
+        }
+
+        [TestMethod]
+        public async Task GetStocks_ShouldReturnEmptyList_WhenHttpResponseIsNotSuccessful()
+        {
+            Mock<ILoggerWrapper> loggerMock = new Mock<ILoggerWrapper>();
+            Mock<IHttpClientWrapper> httpClientMock = new Mock<IHttpClientWrapper>();
+            DataAccess dataAccess = new DataAccess(loggerMock.Object, httpClientMock.Object);
+
+            HttpResponseMessage httpResponse = new HttpResponseMessage(HttpStatusCode.BadRequest);
+
+            httpClientMock.Setup(client => client.SendAsync(It.IsAny<HttpRequestMessage>()))
+                          .ReturnsAsync(httpResponse);
+
+            var result = await dataAccess.GetStocks();
+
+            Assert.IsTrue(result.Count == 0);
+            loggerMock.Verify(logger => logger.LogError(It.IsAny<Exception>(), It.IsAny<string>()), Times.Once);
+            httpClientMock.Verify(client => client.SendAsync(It.IsAny<HttpRequestMessage>()), Times.Once);
+        }
+
+
+
+
+
         //[TestMethod]
         //public async Task GetStocks_ShouldReturnListOfStocks()
         //{
