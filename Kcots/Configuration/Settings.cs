5b3d0906c3c2c52cfa5cf8d18d28c6a85b2a0ffe
scottphillipsmoov <scott.phillips@moov.tv>
2023-12-15 15:02:11 +0000

    Settings class used for ConfigureServices

--- Kcots/Configuration/Settings.cs
+++ Kcots/Configuration/Settings.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Kcots.Data;
+using Kcots.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,38 @@ using System.Threading.Tasks;
 
 namespace Kcots.Configuration
 {
-    class Settings
+    public class Settings
     {
+        private readonly ILoggerWrapper logger;
+        public static IServiceProvider serviceProvider;
+
+        public Settings(ILoggerWrapper logger)
+        {
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            serviceProvider = ConfigureServices();
+        }
+
+        public IServiceProvider ConfigureServices()
+        {
+            //Configure services with interfaces
+            try
+            {
+                logger.LogInformation("Getting Stocks List");
+                // Example configuration using Microsoft.Extensions.DependencyInjection
+                var serviceProvider = new ServiceCollection()
+                    .AddScoped<IDataAccess, DataAccess>()  // Registering the concrete implementation of IDataAccess
+                    .AddScoped<IHttpClientWrapper, HttpClientWrapper>()  // Registering the concrete implementation of IHttpClientWrapper
+                    .AddScoped<ILoggerWrapper, LoggingWrapper>()  // Registering the concrete implementation of ILoggerWrapper
+                    .AddHttpClient()  // Registering HttpClient as a transient service
+                    .BuildServiceProvider();
+
+                return serviceProvider;
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error configuring services");
+                throw;
+            }
+        }
     }
 }
