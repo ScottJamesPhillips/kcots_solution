a01197380b8801a90838bfc49c0257be496236db
ScottJamesPhillips <scottjamesphillips@hotmail.com>
2023-12-18 17:07:13 +0000

    Displaying candle stick data from twelve data

--- Kcots/Controls/HomeTab/CandleStickChart.xaml.cs
+++ Kcots/Controls/HomeTab/CandleStickChart.xaml.cs
@@ -24,73 +24,44 @@ namespace Kcots.Controls.HomeTab
     public partial class CandleStickChart : UserControl
     {
         static List<StocksMarketData> marketData = new List<StocksMarketData>();
-        CandleSeries series = new CandleSeries()
-        {
 
-            ItemsSource = marketData,
-
-            XBindingPath = "DateTime",
-
-            High = "High",
-            Low = "Low",
-
-            Open = "Open",
-            Close = "Close",
-
-            Interior = new SolidColorBrush(Color.FromRgb(0x4A, 0x4A, 0X4A))
-
-        };
         public CandleStickChart()
         {
             InitializeComponent();
-            //DataContext = new ViewModel();
-        }
-
-        public class CandleChartModel
-        {
-            public DateTime Date { get; set; }
-            public double High { get; set; }
-            public double Low { get; set; }
-            public double Open { get; set; }
-            public double Close { get; set; }
-        }
-
-        public class ViewModel
-        {
-            public ViewModel()
-            {
-                this.StockPriceDetails = new ObservableCollection<CandleChartModel>();
-
-                //this.StockPriceDetails.Add(new CandleChartModel() { Date = date.AddDays(2), Open = 861, High = 868.4, Low = 835.2, Close = 843.45 });
-                //this.StockPriceDetails.Add(new CandleChartModel() { Date = date.AddDays(3), Open = 846.15, High = 853, Low = 838.5, Close = 847.5 });
-                //this.StockPriceDetails.Add(new CandleChartModel() { Date = date.AddDays(4), Open = 846, High = 860.75, Low = 841, Close = 855 });
-                //this.StockPriceDetails.Add(new CandleChartModel() { Date = date.AddDays(5), Open = 841, High = 845, Low = 827.85, Close = 838.65 });
-                //this.StockPriceDetails.Add(new CandleChartModel() { Date = date.AddDays(6), Open = 846, High = 874.5, Low = 841, Close = 860.75 });
-                //this.StockPriceDetails.Add(new CandleChartModel() { Date = date.AddDays(7), Open = 865, High = 872, Low = 865, Close = 868.9 });
-
-                foreach(StocksMarketData md in marketData)
-                {
-                    this.StockPriceDetails.Add(new CandleChartModel() { Date = md.DateTime, Open = decimal.ToDouble(md.Open), High = decimal.ToDouble(md.High), Low = decimal.ToDouble(md.Low), Close = decimal.ToDouble(md.Close) });
-                }
-            }
-            public ObservableCollection<CandleChartModel> StockPriceDetails { get; set; }
         }
 
         private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
-            //On change datacontext (change stock selected)
             marketData = e.NewValue as List<StocksMarketData>;
             if (marketData != null)
             {
-                DataContext = new ViewModel();
+                InitializeCandleSeries();
             }
-            //foreach (StocksMarketData s in marketData)
-            //{
+        }
 
-            //}
+        private void InitializeCandleSeries()
+        {
+            try
+            {
+                //Clear exisitng chart series
+                candleStickChart.Series = new ChartSeriesCollection();
+                CandleSeries series = new CandleSeries()
+                {
+                    ItemsSource = marketData,
+                    XBindingPath = "DateTime",
+                    High = "High",
+                    Low = "Low",
+                    Open = "Open",
+                    Close = "Close"
+                };
+                // Add the series to the chart
+                candleStickChart.Series.Add(series);
+                //series.ComparisonMode = Syncfusion.UI.Xaml.Charts.FinancialPrice.Open;
+            }
+            catch(Exception ex)
+            {
 
-            //if (selectedStock != null)
-            //    GetCurrentStockInfo();
+            }
         }
     }
 }
